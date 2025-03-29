9f14d2ab503bd604a1d8484c2f7380c5ec94f0ea
EkTanjiro <132507005+EkTanjiro@users.noreply.github.com>
2025-03-28 22:27:39 -0700

    Update apps-view.tmpl.php
    Fixed the name column to match name in database.

--- extras/apps-view.tmpl.php
+++ extras/apps-view.tmpl.php
@@ -10,64 +10,70 @@
     <div class="clear"></div>
 
     <?php
-    $app = new Application();
-    $staffapps = $app->getStaffApps();
-    $page = ($_GET['p'] && is_numeric($_GET['p'])) ? $_GET['p'] : 1;
-    $count = count($staffapps);
+    // Get all installed plugins directly.
+    $installedPlugins = $ost->plugins->allInstalled();
+    $count = count($installedPlugins);
+    $page = (isset($_GET['p']) && is_numeric($_GET['p'])) ? $_GET['p'] : 1;
     $pageNav = new Pagenate($count, $page, PAGE_LIMIT);
     $pageNav->setURL('apps.php');
-    $showing=$pageNav->showing().' '._N('application', 'applications', $count);
+    $showing = $pageNav->showing() . ' ' . _N('application', 'applications', $count);
     ?>
 
     <form action="" method="POST" name="">
         <?php csrf_token(); ?>
-        <input type="hidden" name="do" value="mass_process" >
-        <input type="hidden" id="action" name="a" value="" >
+        <input type="hidden" name="do" value="mass_process">
+        <input type="hidden" id="action" name="a" value="">
         <table class="list" border="0" cellspacing="1" cellpadding="0" width="940">
             <thead>
             <tr>
                 <th width="55%"><?php echo __('Application Name'); ?></th>
                 <th width="10%"><?php echo __('Version'); ?></th>
                 <th width="20%"><?php echo __('Date Installed'); ?></th>
-                <?php if($thisstaff->isAdmin()) { ?>
+                <?php if ($thisstaff->isAdmin()) { ?>
                     <th width="15%"><?php echo __('Plugin Settings'); ?></th>
                 <?php } ?>
             </tr>
             </thead>
             <tbody>
             <?php
-            foreach ($staffapps as $p) {
-                    foreach ($ost->plugins->allInstalled() as $a) { ?>
-                    <tr>
-                        <td><a href="<?php echo $p['href']; ?>">
-                                <?php echo $p['desc']; ?></a></td>
-                        <td><?php echo $a->getVersion(); ?></a></td>
-                        <td><?php echo Format::datetime($a->getInstallDate()); ?></td>
-                        <?php if($thisstaff->isAdmin()) {
-                            if ($a->getName() == $p['desc']) { ?>
-                            <td>
-                                <a class="button" href="plugins.php?id=<?php echo $a->getId() ?>"><?php echo __('Settings'); ?></a>
-                            </td>
-                        <?php } } ?>
-                    </tr>
-            <?php } } ?>
+            // Loop over installed plugins instead of nesting two loops.
+            foreach ($installedPlugins as $a) { ?>
+                <tr>
+                    <td>
+                        <a href="plugins.php?id=<?php echo $a->getId(); ?>">
+                            <?php echo $a->getName(); ?>
+                        </a>
+                    </td>
+                    <td><?php echo $a->getVersion(); ?></td>
+                    <td><?php echo Format::datetime($a->getInstallDate()); ?></td>
+                    <?php if ($thisstaff->isAdmin()) { ?>
+                        <td>
+                            <a class="button" href="plugins.php?id=<?php echo $a->getId(); ?>">
+                                <?php echo __('Settings'); ?>
+                            </a>
+                        </td>
+                    <?php } ?>
+                </tr>
+            <?php } ?>
             </tbody>
             <tfoot>
             <tr>
                 <td colspan="5">
-                    <?php if(!$count) {
+                    <?php 
+                    if (!$count) {
                         echo sprintf(__('No plugins installed yet &mdash; %s add one %s!'),
                             '<a href="?a=add">', '</a>');
                     } else {
                         echo $showing;
-                    } ?>
+                    } 
+                    ?>
                 </td>
             </tr>
             </tfoot>
         </table>
         <?php
-        if ($count) //Show options..
-            echo '<div>&nbsp;'.__('Page').':'.$pageNav->getPageLinks().'&nbsp;</div>';
+        if ($count) {
+            echo '<div>&nbsp;' . __('Page') . ':' . $pageNav->getPageLinks() . '&nbsp;</div>';
+        }
         ?>
-
     </form>
